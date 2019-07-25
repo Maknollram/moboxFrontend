a1a6baeccd26d3b7b7e63cc4b34fb45239a89d21
Marllon <marllon@amrj.mar.mil.br>
2019-07-25 14:02:17 -0300

    inserçao de um modal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,12 @@ export default class Main extends Component {
         newBox: ''
     }
 
+    componentDidMount(){
+        let modal = document.getElementById('modal')
+            
+        modal.style.display = "none"
+    }
+
     handleSubmit = async e  => {
         
         e.preventDefault()
@@ -31,16 +37,39 @@ export default class Main extends Component {
         this.props.history.push('/list')
 
     }
+
+    modal = (tipo)  => {
+        
+        let modal = document.getElementById('modal')
+            
+        modal.style.display = tipo
+
+    }
     
     render() {
         return (
             <div id="main-container">
                 <form onSubmit={this.handleSubmit}>
-                    <img src={logo} alt="moboxLogo" onClick={this.handleBoxes}/>
+                    <img src={logo} alt="moboxLogo" onClick={() => this.modal('block')}/>
                     <input value={this.state.newBox} onChange={this.handleInputChange} maxLength="25" placeholder="Digite o nome para a box" />
                     <button type="submit">Criar</button>
                     <label onClick={this.handleBoxes}>Clique aqui para ver as mobox existentes.</label>
                 </form>
+                <div id="modal" class="modalPrivado modalContainer">
+                    <div class="modal-content">
+                        <div align="center">
+                            <p>
+                                <b>Criado por Maknollram</b>
+                                <br/>
+                                <b>Mak Inc.</b>
+                                <br/>
+                                <b>2019</b>
+                                <br/>
+                                <button id="modalButton" onClick={() => this.modal('none')}>Fechar</button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
